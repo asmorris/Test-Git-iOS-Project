1c662add3534bfc57f74b4148286540351f05c21
Andrew Morrison <andrewmorrison@Andrews-MacBook-Pro-2.local>
2014-08-29 12:20:58 -0400

    added test class

---
+++ GitTest/STATest.h
@@ -0,0 +1,13 @@
+//
+//  STATest.h
+//  GitTest
+//
+//  Created by Andrew Morrison on 8/28/14.
+//  Copyright (c) 2014 SharkTankApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STATest : NSObject
+
+@end
---
+++ GitTest/STATest.m
@@ -0,0 +1,13 @@
+//
+//  STATest.m
+//  GitTest
+//
+//  Created by Andrew Morrison on 8/28/14.
+//  Copyright (c) 2014 SharkTankApps. All rights reserved.
+//
+
+#import "STATest.h"
+
+@implementation STATest
+
+@end
